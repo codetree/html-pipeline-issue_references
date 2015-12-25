303dbbe55e66723f469584fb30cee39d66076c3a
Derrick Reimer <derrickreimer@gmail.com>
2015-12-25 12:50:19 -0800

    Basic implementation

--- lib/html/pipeline/issue_reference_filter.rb
+++ lib/html/pipeline/issue_reference_filter.rb
@@ -6,8 +6,91 @@ module HTML
     class IssueReferenceFilter < Filter
       VERSION = "0.1.0".freeze
 
-      def call
+      REPOSITORY_NAME = /[a-z0-9][a-z0-9\-]*\/[a-z0-9][a-z0-9\-_]*/ix
 
+      # Match references of the form:
+      #
+      # - #123
+      # - codetree/feedback#123
+      # - GH-123
+      #
+      # See http://rubular.com/r/evB7RlvUfI
+      SHORT_PATTERN = /
+        (?:^|\W)                    # beginning of string or non-word char
+        (
+          (?:(#{REPOSITORY_NAME})?    # repository name (optional)
+          \#|(?:GH\-))(\d+)           # issue number
+        )
+        (?=
+          \.+[ \t\W]|               # dots followed by space or non-word character
+          \.+$|                     # dots at end of line
+          [^0-9a-zA-Z_.]|           # non-word character except dot
+          $                         # end of line
+        )
+      /ix
+
+      # Match references of the form:
+      #
+      # - https://github.com/codetree/feedback/issues/123
+      # - https://github.com/codetree/feedback/pulls/123
+      URL_PATTERN = /
+        (?:^|\W)                    # beginning of string or non-word char
+        https:\/\/github.com\/
+        (#{REPOSITORY_NAME})        # repository name
+        \/(?:issues|pulls)\/
+        (\d+)                       # issue number
+        (?=
+          \.+[ \t\W]|               # dots followed by space or non-word character
+          \.+$|                     # dots at end of line
+          [^0-9a-zA-Z_.]|           # non-word character except dot
+          $                         # end of line
+        )
+      /ix
+
+      # Don't look for mentions in text nodes that are children of these elements
+      IGNORE_PARENTS = %w(pre code a style).to_set
+
+      def self.issue_references_in(text)
+        text.gsub SHORT_PATTERN do |match|
+          repository = $2
+          number = $3
+          yield match, repository, number
+        end
+      end
+
+      def default_repo
+        context[:repository]
+      end
+
+      def call
+        doc.search('.//text()').each do |node|
+          content = node.to_html
+          next if has_ancestor?(node, IGNORE_PARENTS)
+          html = issue_reference_filter(content, base_url, default_repo)
+          next if html == content
+          node.replace(html)
+        end
+
+        doc
+      end
+
+      def issue_reference_filter(text, base_url = '/', default_repo = nil)
+        self.class.issue_references_in(text) do |match, referenced_repo, number|
+          repo = referenced_repo || default_repo
+          leading_whitespace = match.start_with?(' ') ? ' ' : ''
+          repo ? leading_whitespace + link_to_issue(repo, number, default_repo) : match
+        end
+      end
+
+      def link_to_issue(repo, number, default_repo)
+        content = "##{number}"
+        content = "#{repo}#{content}" if repo != default_repo
+
+        url = base_url.dup
+        url << "/" unless url =~ /[\/~]\z/
+        url << "#{repo}/issues/#{number}"
+
+        "<a href='#{url}' class='issue-reference'>#{content}</a>"
       end
     end
   end
---
+++ test/html/pipeline/issue_reference_filter_test.rb
@@ -0,0 +1,74 @@
+require 'test_helper'
+
+class HTML::Pipeline::IssueReferenceFilterTest < Minitest::Test
+  IssueReferenceFilter = HTML::Pipeline::IssueReferenceFilter
+
+  def filter(html, base_url = '/', default_repo = 'foo/bar')
+    IssueReferenceFilter.call(html, base_url: base_url, repository: default_repo)
+  end
+
+  def test_filtering_a_documentfragment
+    body = "<p>Fixes #123</p>"
+    doc  = Nokogiri::HTML::DocumentFragment.parse(body)
+
+    res  = filter(doc, '/', 'foo/bar')
+    assert_same doc, res
+
+    link = "<a href=\"/foo/bar/issues/123\" class=\"issue-reference\">#123</a>"
+    assert_equal "<p>Fixes #{link}</p>",
+      res.to_html
+  end
+
+  def test_filtering_plain_text
+    body = "<p>Fixes #123</p>"
+    res  = filter(body, '/', 'foo/bar')
+
+    link = "<a href=\"/foo/bar/issues/123\" class=\"issue-reference\">#123</a>"
+    assert_equal "<p>Fixes #{link}</p>",
+      res.to_html
+  end
+
+  def test_not_replacing_references_in_pre_tags
+    body = "<pre>#123</pre>"
+    assert_equal body, filter(body).to_html
+  end
+
+  def test_not_replacing_references_in_code_tags
+    body = "<p><code>#123</code></p>"
+    assert_equal body, filter(body).to_html
+  end
+
+  def test_not_replacing_references_in_style_tags
+    body = "<style>#123 { color: red; }</style>"
+    assert_equal body, filter(body).to_html
+  end
+
+  def test_not_replacing_references_in_links
+    body = "<p><a>#123</a> okay</p>"
+    assert_equal body, filter(body).to_html
+  end
+
+  def test_not_replacing_references_without_repo
+    body = "<p>Fixes #123</p>"
+    res  = filter(body, '/', nil)
+    assert_equal body, res.to_html
+  end
+
+  def test_including_referenced_repo_when_different_than_default
+    body = "<p>Fixes bar/baz#123</p>"
+    res  = filter(body, '/', 'foo/bar')
+
+    link = "<a href=\"/bar/baz/issues/123\" class=\"issue-reference\">bar/baz#123</a>"
+    assert_equal "<p>Fixes #{link}</p>",
+      res.to_html
+  end
+
+  def test_including_referenced_repo_when_default_is_not_set
+    body = "<p>Fixes bar/baz#123</p>"
+    res  = filter(body, '/', nil)
+
+    link = "<a href=\"/bar/baz/issues/123\" class=\"issue-reference\">bar/baz#123</a>"
+    assert_equal "<p>Fixes #{link}</p>",
+      res.to_html
+  end
+end
