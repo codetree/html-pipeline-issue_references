3e9bcd85928bddcb10ab6e7a3a63d0be9ee824c0
Andrew Conrad <andrew@versyy.com>
2020-04-21 11:30:24 -0500

    issue-7: add auto-corrected syntax

--- lib/html/pipeline/issue_reference_filter.rb
+++ lib/html/pipeline/issue_reference_filter.rb
@@ -1,10 +1,12 @@
-require "html/pipeline"
-require "set"
+# frozen_string_literal: true
+
+require 'html/pipeline'
+require 'set'
 
 module HTML
   class Pipeline
     class IssueReferenceFilter < Filter
-      REPOSITORY_NAME = /[a-z0-9][a-z0-9\-]*\/[a-z0-9][a-z0-9\-_]*/ix
+      REPOSITORY_NAME = %r{[a-z0-9][a-z0-9\-]*/[a-z0-9][a-z0-9\-_]*}ix.freeze
 
       # Match references of the form:
       #
@@ -25,17 +27,17 @@ module HTML
           [^0-9a-zA-Z_.]|           # non-word character except dot
           $                         # end of line
         )
-      /ix
+      /ix.freeze
 
       # Match references of the form:
       #
       # - https://github.com/codetree/feedback/issues/123
       # - https://github.com/codetree/feedback/pulls/123
-      URL_PATTERN = /
+      URL_PATTERN = %r{
         (?:^|\W)                    # beginning of string or non-word char
-        https:\/\/github.com\/
+        https://github.com/
         (#{REPOSITORY_NAME})        # repository name
-        \/(?:issues|pulls)\/
+        /(?:issues|pulls)/
         (\d+)                       # issue number
         (?=
           \.+[ \t\W]|               # dots followed by space or non-word character
@@ -43,15 +45,15 @@ module HTML
           [^0-9a-zA-Z_.]|           # non-word character except dot
           $                         # end of line
         )
-      /ix
+      }ix.freeze
 
       # Don't look for mentions in text nodes that are children of these elements
-      IGNORE_PARENTS = %w(pre code a style).to_set
+      IGNORE_PARENTS = %w[pre code a style].to_set
 
       def self.issue_references_in(text)
         text.gsub SHORT_PATTERN do |match|
-          repository = $2
-          number = $3
+          repository = Regexp.last_match(2)
+          number = Regexp.last_match(3)
           yield match, repository, number
         end
       end
@@ -64,15 +66,17 @@ module HTML
         doc.search('.//text()').each do |node|
           content = node.to_html
           next if has_ancestor?(node, IGNORE_PARENTS)
+
           html = issue_reference_filter(content, base_url, default_repo)
           next if html == content
+
           node.replace(html)
         end
 
         doc
       end
 
-      def issue_reference_filter(text, base_url = '/', default_repo = nil)
+      def issue_reference_filter(text, _base_url = '/', default_repo = nil)
         self.class.issue_references_in(text) do |match, referenced_repo, number|
           repo = referenced_repo || default_repo
           repo ? link_to_issue(repo, number, default_repo) : match
@@ -84,7 +88,7 @@ module HTML
         content = "#{repo}#{content}" if repo != default_repo
 
         url = base_url.dup
-        url << "/" unless url =~ /[\/~]\z/
+        url << '/' unless url =~ %r{[/~]\z}
         url << "#{repo}/issues/#{number}"
 
         "<a href='#{url}' class='issue-reference'>#{content}</a>"
--- lib/html/pipeline/issue_references/version.rb
+++ lib/html/pipeline/issue_references/version.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module HTML
   class Pipeline
     class IssueReferences
-      VERSION = '1.0.2'.freeze
+      VERSION = '1.0.2'
     end
   end
 end
